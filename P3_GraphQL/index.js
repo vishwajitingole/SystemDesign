5ba6a6607cf1c9ffeda3352cc88d68ec57753a2e
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2024-07-07 19:50:41 +0530

    graphql

---
+++ P3_GraphQL/index.js
@@ -0,0 +1,7 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
